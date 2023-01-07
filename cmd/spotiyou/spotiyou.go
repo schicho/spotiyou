7a64975ee4e79517d2ef1c0c9a1d3443bc7a9ece
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2023-01-07 21:17:07 +0300

    sample programm to query every 3 minutes

---
+++ cmd/spotiyou/spotiyou.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/schicho/spotiyou/internal/telegram"
+	"github.com/schicho/spotiyou/pkg/api"
+	"github.com/schicho/spotiyou/pkg/spotiyou"
+)
+
+func main() {
+	var telegramToken string
+	var telegramChatID string
+	var spotifyClientID string
+	var spotifyClientSecret string
+
+	telegramToken = os.Getenv("TELEGRAM_TOKEN")
+	telegramChatID = os.Getenv("TELEGRAM_CHAT_ID")
+	spotifyClientID = os.Getenv("SPOTIFY_CLIENT_ID")
+	spotifyClientSecret = os.Getenv("SPOTIFY_CLIENT_SECRET")
+
+	if telegramToken == "" || telegramChatID == "" || spotifyClientID == "" || spotifyClientSecret == "" {
+		log.Fatal("missing environment variables")
+	}
+
+	teleNotifier := telegram.NewTelegramNotifier(telegramToken, telegramChatID)
+	spotifyApi, err := api.New(spotifyClientID, spotifyClientSecret)
+	if err != nil {
+		panic(err)
+	}
+
+	spot := spotiyou.NewSpotiyou(spotifyApi, teleNotifier)
+
+	spot.AddUser("spotify") // add user to spotiyou by userID (can be found in the URL of the user's profile)
+
+	ticker := time.NewTicker(3 * time.Minute)
+	for range ticker.C {
+		err := spot.SpotAllUsers()
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
