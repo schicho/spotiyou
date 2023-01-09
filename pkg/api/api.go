b771182d225d00e3a116a7e7021ff0a26ab082cb
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2023-01-09 18:59:53 +0300

    renew token automatically

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"log"
+	"sync/atomic"
+	"time"
+
 	spotiyou "github.com/schicho/spotiyou/internal/spotify"
 	"github.com/schicho/spotiyou/pkg/playlist"
 )
@@ -8,7 +12,7 @@ import (
 // API is the access point to the Spotify API.
 // It wraps the internal/spotify package.
 type API struct {
-	sc *spotiyou.SpotifyClient
+	sc atomic.Pointer[spotiyou.SpotifyClient]
 }
 
 func New(clientID, clientSecret string) (*API, error) {
@@ -17,11 +21,29 @@ func New(clientID, clientSecret string) (*API, error) {
 		return nil, err
 	}
 
-	return &API{
-		sc: spotifyClient,
-	}, nil
+	var api API = API{}
+	api.sc.Store(spotifyClient)
+
+	// renew token every 45 minutes
+	// this is a limitation of the auth mechanism of
+	// the library.
+	go func() {
+		t := time.NewTicker(45 * time.Minute)
+		for {
+			<-t.C
+			spotifyClient, err := spotiyou.NewSpotifyClient(clientID, clientSecret)
+			if err != nil {
+				log.Println("error renewing token:", err)
+				continue
+			}
+			api.sc.Store(spotifyClient)
+		}
+	}()
+
+	return &api, nil
 }
 
 func (a *API) GetUserPlaylists(userID string) ([]playlist.BasicPlaylist, error) {
-	return a.sc.GetUserPlaylists(userID)
+	spotifyClient := a.sc.Load()
+	return spotifyClient.GetUserPlaylists(userID)
 }
